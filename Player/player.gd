3487da3a1886f0ebdbd302b0908cb5ffca478c8a
ossi1801 <ossihakki@gmail.com>
2024-06-30 20:01:25 +0300

    dashing

--- Player/player.gd
+++ Player/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 @onready var animations = $AnimationPlayer
 @export var player_speed:float = 150
+@export var player_dash_multiplier: float = 5;
+var player_dash:float = 1;
+var player_can_dash:bool=true;
 #@export var player_acceleration_speed:float = 1  
 
 func  movement():
 	var move_dir = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	velocity = move_dir*player_speed 
+	if(Input.is_action_just_pressed("Dash")):
+		dash()
+	velocity = (move_dir*player_speed) * player_dash;
 	#velocity.x = move_toward(velocity.x,player_speed*move_dir.x,player_acceleration_speed)
 	#velocity.y = move_toward(velocity.y,player_speed*move_dir.y,player_acceleration_speed)
 	#No need for delta wtf???? *delta
@@ -22,6 +27,22 @@ func animate_movement():
 		elif 	velocity.y < 0: dir = "up"
 		
 		animations.play("walk_"+dir)
+	
+func dash():
+	if player_can_dash:
+		player_dash = player_dash_multiplier;
+		#Todo set immortality here to true to create "iframe"
+		
+		#Reset dash and set immortality to false after x amount of time
+		#await returns from function until timer done and return to point where it returned
+		await get_tree().create_timer(0.1).timeout 
+		player_dash = 1;
+		player_can_dash = false;
+		#Then set cool down for dash (this can be done as animation later
+		await get_tree().create_timer(0.3).timeout
+		player_can_dash = true;
+		
+		
 	
 func _physics_process(delta):
 	movement()
